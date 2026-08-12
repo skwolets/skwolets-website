bc613f281cfca2089a018e8db27fb091119519b3
skwolets <kovalenkonikitahere@gmail.com>
2026-08-12 05:24:26 +0400

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,23 @@ header.style.background =
 
 
 });
+
+const marqueeTrack = document.querySelector(".marquee-track");
+
+if (marqueeTrack) {
+
+    const items = Array.from(marqueeTrack.children);
+
+    for (let i = items.length - 1; i > 0; i--) {
+
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [items[i], items[j]] = [items[j], items[i]];
+
+    }
+
+    items.forEach(item => {
+        marqueeTrack.appendChild(item);
+    });
+
+}
