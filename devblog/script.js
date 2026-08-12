b6e81a66ab9202f28fb6e67960aced55011b628d
skwolets <kovalenkonikitahere@gmail.com>
2026-08-12 23:35:48 +0400

    Update script.js

--- devblog/script.js
+++ devblog/script.js
@@ -17,3 +17,24 @@ if (marqueeTrack) {
     });
 
 }
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 500) {
+        backToTop.classList.add("visible");
+    } else {
+        backToTop.classList.remove("visible");
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
