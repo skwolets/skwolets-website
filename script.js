a18aac353462e6a8bc4424456572884afaea41ea
skwolets <kovalenkonikitahere@gmail.com>
2026-08-12 05:46:37 +0400

    Update script.js

--- script.js
+++ script.js
@@ -54,23 +54,3 @@ header.style.background =
 
 
 });
-
-const marqueeTrack = document.querySelector(".marquee-track");
-
-if (marqueeTrack) {
-
-    const items = Array.from(marqueeTrack.children);
-
-    for (let i = items.length - 1; i > 0; i--) {
-
-        const j = Math.floor(Math.random() * (i + 1));
-
-        [items[i], items[j]] = [items[j], items[i]];
-
-    }
-
-    items.forEach(item => {
-        marqueeTrack.appendChild(item);
-    });
-
-}
